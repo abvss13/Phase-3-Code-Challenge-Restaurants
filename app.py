b8044add89e953abe8248a902c3856992fd5454e
abvss13 <abdullahi.abass@student.moringaschool.com>
2023-12-05 09:28:50 +0300

    done

--- app.py
+++ app.py
@@ -3,8 +3,8 @@
 from customers import Customer
 from restaurants import Restaurant
 
-customer1 = Customer("Harry", "Maguire") 
-restaurant1 = Restaurant("The Plaza")
+customer1 = Customer("Abdullahi", "Abass") 
+restaurant1 = Restaurant("The Cafe Java")
 
 print(customer1.full_name())
 print(restaurant1.name)
